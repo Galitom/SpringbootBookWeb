f6fbd17d69fd82bb6406c4bf6025dd55b9979986
Tommaso Galimberti <galimba615@gmail.com>
2024-01-19 09:34:00 +0100

    feat: aggiunta grafica

--- src/main/java/org/example/firstwebauth/Model/BookRepository.java
+++ src/main/java/org/example/firstwebauth/Model/BookRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<Book, Integer> {
-
     @Query("SELECT b FROM Book b JOIN b.users u WHERE u.id = :userId")
     List<Book> findBooksByUserId(@Param("userId") Integer userId);
-
 }
